a6f3792a87d82ace91d716c617d1b1bade861345
allan <katana_kaj@hotmail.com>
2015-09-30 23:57:37 +0200

    Implementet Player

---
+++ PlayerScript.js
@@ -0,0 +1,31 @@
+﻿var videoplayer = document.getElementById("media");
+videoplayer.addEventListener('timeupdate', updatebar, false);
+
+function playpause() {
+    if (videoplayer.paused)
+        videoplayer.play();
+    else
+        videoplayer.pause()
+}
+function mute() {
+    if (videoplayer.muted)
+        videoplayer.muted = false;
+    else
+        videoplayer.muted = true;
+}
+function fullscreenmode() {
+    if (videoplayer.requestFullscreen) {
+        videoplayer.requestFullscreen();
+    } else if (videoplayer.msRequestFullscreen) {
+        videoplayer.msRequestFullscreen();
+    } else if (videoplayer.mozRequestFullScreen) {
+        videoplayer.mozRequestFullScreen();
+    } else if (videoplayer.webkitRequestFullscreen) {
+        videoplayer.webkitRequestFullscreen();
+    }
+}
+function updatebar() {
+    var progbar = document.getElementById("mediaprogress");
+    var percentage = Math.floor((100 / videoplayer.duration) * videoplayer.currentTime);
+    progbar.value = percentage;
+}
